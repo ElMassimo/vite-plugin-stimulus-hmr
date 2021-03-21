432c508b42fd8bdec40386f888afa4ad0e99af1d
Maximo Mussini <maximomussini@gmail.com>
2021-03-21 10:49:30 -0300

    release: vite-plugin-stimulus-hmr@1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
-## vite-plugin-stimulus-hmr 1.0.0 (2020-02-15)
+## vite-plugin-stimulus-hmr 1.0.1 (2021-03-21)
+
+- Add keywords and fix repo URL.
+
+## vite-plugin-stimulus-hmr 1.0.0 (2021-02-15)
 
 - Initial version
--- example/controllers/index.ts
+++ example/controllers/index.ts
@@ -6,5 +6,5 @@ import { registerControllers } from 'stimulus-vite-helpers'
 const app = Application.start()
 
 // Controller files must be named *_controller.js.
-export const controllers = import.meta.globEager('./**/*_controller.js')
+export const controllers = import.meta.globEager('../**/*_controller.js')
 registerControllers(app, controllers)
